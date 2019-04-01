5967c98bf4742695a2c27872fcb0f8572431fbc6
Ben Tilghman <ben.tilghman@gmail.com>
2019-04-01 11:40:30 -0400

    step 1

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+/* global shoppingList, cuid */
+
+// eslint-disable-next-line no-unused-vars
+
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+  shoppingList.render();
+});
+
+console.log(store);
+console.log(foo);
